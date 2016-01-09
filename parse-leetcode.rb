fb5b62a49ea7f1a0bc18f0945e41f80c75621923
LIU Yang <JeremyRobturtle@gmail.com>
2016-01-08 22:33:38 -0500

    add script description

--- parse-leetcode.rb
+++ parse-leetcode.rb
@@ -2,16 +2,25 @@
 # fetch the href from the leetcode.com/problemset/algorithms/ (done by wget tr and grep href)
 # create a file named `problem-name.md' and heading with href link.
 # ./parse-leetcode.rb hrefs.txt
-LEET_CODE_SITE = 'https://leetcode.com'
+#
+# input file format: each line contains one href element
+# example: <a href="/relative/name.html">Title</a>
+# 
+# The script just concat the BASE_URL with href link, and create a file named `problem-name.md' with a markdown link:
+# output:
+# [Title](https://leetcode.com/relative/name.html)
+#
+# change the `BASE_URL' and the no's initial value and iterating direction to process other hrefs
+BASE_URL = 'https://leetcode.com'
+no = 0
 
 href_fname = ARGV[0]
-no = 0
 File.foreach(href_fname) {|line| no += 1}
 
 puts "line number: #{no}"
 File.foreach(href_fname) do |line|
   %r{<a href="(?<link>.+/(?<name>.+?)/)">(?<title>.+?)</a>} =~ line
-  puts "fname: #{no.to_s.rjust(3, '0')}-#{name}.md; title: #{no}. #{title}; link: #{LEET_CODE_SITE}#{link}"
-  File.open("#{no.to_s.rjust(3, '0')}-#{name}.md", 'w') {|f| f.write "[#{no}. #{title}](#{LEET_CODE_SITE}#{link})\n"}
+  puts "fname: #{no.to_s.rjust(3, '0')}-#{name}.md; title: #{no}. #{title}; link: #{BASE_URL}#{link}"
+  File.open("#{no.to_s.rjust(3, '0')}-#{name}.md", 'w') {|f| f.write "[#{no}. #{title}](#{BASE_URL}#{link})\n"}
   no -= 1
 end
